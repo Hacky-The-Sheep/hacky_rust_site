3f1c38c7e810a121f630dc40f0c5739ff2db6a30
Hacky <Jon.Nguyen7@protonmail.com>
2023-03-19 23:17:37 -0500

    added link to Catppuccin in the footer. Dedicated the page to Our Lady

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ fn app() -> Html {
         <>
             <h1>{ "Welcome!" }</h1>
             <p>{ "This landing page was written in Rust 🚀 using Yew" }</p>
-            <h2>{ "Enjoy this picture of a sheep... Mr Robot Style" }</h2>
-            <img class="hacky" src="https://github.com/Hacky-The-Sheep/hacky_rust_site/blob/main/media/Hacky_robot.png?raw=true"/>
+            <h2>{ "This site is dedicated to Our Lady of Perpetual Help" }</h2>
+            <img class="hacky" src="https://raw.githubusercontent.com/Hacky-The-Sheep/hacky_rust_site/main/media/OLPH.jpeg"/>
+            <br/>
             <p>{ "The page will be added on as I continue to learn Rust and Web development. Adding to it, I believe I can keep this site
             to simply a page, with links to other platforms. Although, I think that adding links to single (smaller) sites I find for others
             to view will still be a goal. Just need to determine how to add that to the page... 🤔 
@@ -17,12 +18,13 @@ fn app() -> Html {
             <div class="sample_items">
                 {
                     items.into_iter().map(|items| {
-                        html!{<div key={items}>{ format!("{items}") }</div>}
+                        html!{<div key={items}>{ format!("• {items}") }</div>}
                     }).collect::<Html>()
                 }
             </div>
-            <p>{ "Or, I can host the sites on some SQL (rust based) DB and do a call to pull a few of them at random. That might be an issue
+            <p>{ "Or, I can host the sites on SurrealDB and do a call to pull a few of them at random. That might be an issue
             with scaling in the future but I only have 3 sites in mind right now so that will be future Hackys problem I suppose."}</p>
+            <a class="cat" href="https://github.com/catppuccin/catppuccin">{"Themed with Catppuccin 🐱"}</a>
         </>
     }
 }
